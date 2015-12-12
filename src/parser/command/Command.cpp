7977449f1757dedc8e2e8bc385c05766b33d62ed
Bobby Lin <bobbylinjh@gmail.com>
2015-12-13 02:31:01 +0800

    Added constructor and splitString() in Command.cpp .h

--- src/parser/command/Command.cpp
+++ src/parser/command/Command.cpp
@@ -2,13 +2,61 @@
 // Created by Bobby Lin on 12/12/15.
 //
 
+#include <iostream>
+#include <string.h>
 #include "Command.h"
 
-Command::Command() {
-    // TODO Auto-generated constructor stub
-
+Command::Command(string input) {
+    const char * c = input.c_str();
+    vector<string> param = splitString(c,' ');
+    type = param.at(0);
+    if(strcmp(type.c_str(),"save") == 0) {
+        amount = std::stod(param.at(1));
+    }
+    else if(strcmp(type.c_str(),"spend") == 0) {
+        amount = std::stod(param.at(1));
+        expense = param.at(2);
+    }
+    else if(strcmp(type.c_str(),"earn") == 0) {
+        amount = std::stod(param.at(1));
+    }
+    else {
+        type = "invalid";
+    }
 }
 
 Command::~Command() {
-    // TODO Auto-generated destructor stub
+
+}
+
+string Command::getType() {
+    return type;
+}
+
+double Command::getAmount() {
+    return amount;
+}
+
+string Command::getExpense() {
+    return expense;
+}
+
+vector<string> Command::splitString(string const &str, char const delimiter) {
+    vector<string> vec;
+    string element;
+    for_each(begin(str),end(str),[&](char const ch) {
+        if(ch!=delimiter) {
+            element+=ch;
+        }
+        else {
+            if (element.length()>0) {
+                vec.push_back(element);
+                element.clear();
+            }
+        }
+    });
+    if (element.length()>0) {
+        vec.push_back(element);
+    }
+    return vec;
 }
--- src/parser/command/Command.h
+++ src/parser/command/Command.h
@@ -3,6 +3,10 @@
 //
 
 #include <string>
+#include <stdio.h>
+#include <vector>
+#include <regex>
+
 #ifndef SPENDTRACKER_COMMAND_H
 #define SPENDTRACKER_COMMAND_H
 using namespace std;
@@ -10,9 +14,16 @@ using namespace std;
 class Command {
     private:
         string type;
+        double amount;
+        string expense;
     public:
-        Command();
+        Command(string input);
         virtual ~Command();
+
+        string getType();
+        double getAmount();
+        string getExpense();
+        vector<string> splitString(string const &str, char const delimiter);
 };
 
 
