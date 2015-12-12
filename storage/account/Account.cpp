6393f37a639c8caac72aec256a9bbfbe63020c4d
jun159 <baojun159@gmail.com>
2015-12-12 22:15:03 +0800

    Added Account object to represent data

---
+++ storage/account/Account.cpp
@@ -0,0 +1,69 @@
+//
+// Created by Bao Jun on 12/12/15.
+//
+
+#include <cassert>
+#include "Account.h"
+
+Account::Account() {
+    id_ = 0;
+    balance_ = 0;
+    income_ = 0;
+}
+
+Account::Account(int id, int balance, int income,
+                 std::map<Account::Category, int> expense, std::string date) {
+
+    assert(id >= 0);
+    assert(income >= 0);
+
+    id_ = id;
+    balance_ = balance;
+    income_ = income;
+    expense_ = expense;
+    date_ = date;
+}
+
+Account::~Account() {
+
+}
+
+const int& Account::id() const {
+    return id_;
+}
+
+const int& Account::balance() const {
+    return balance_;
+}
+
+const int& Account::income() const {
+    return income_;
+}
+
+const std::map<Account::Category, int>& Account::expense() const {
+    return expense_;
+}
+
+const std::string& Account::date() const {
+    return date_;
+}
+
+void Account::id(const int& id) {
+    id_ = id;
+}
+
+void Account::balance(const int& balance) {
+    balance_ = balance;
+}
+
+void Account::income(const int& income) {
+    income_ = income;
+}
+
+void Account::expense(const std::map<Account::Category, int>& expense) {
+    expense_ = expense;
+}
+
+void Account::date(const std::string& date) {
+    date_ = date;
+}
---
+++ storage/account/Account.h
@@ -0,0 +1,43 @@
+//
+// Created by Bao Jun on 12/12/15.
+//
+
+#include <map>
+#include <string>
+
+#ifndef SPENDTRACKER_ACCOUNT_H
+#define SPENDTRACKER_ACCOUNT_H
+
+
+class Account {
+public:
+    enum Category {
+        CLOTHES, ENTERTAINMENT, FOOD, HEALTH, TRANSPORT
+    };
+
+    Account();
+    Account(int id, int balance, int income,
+            std::map<Account::Category, int> expense, std::string date);
+    virtual ~Account();
+
+    const int& id() const;
+    const int& balance() const;
+    const int& income() const;
+    const std::map<Account::Category, int>& expense() const;
+    const std::string& date() const;
+
+    void id(const int& id);
+    void balance(const int& balance);
+    void income(const int& income);
+    void expense(const std::map<Account::Category, int>& expense);
+    void date(const std::string& date);
+
+private:
+    int id_;
+    int balance_;
+    int income_;
+    std::map<Account::Category, int> expense_;
+    std::string date_;
+};
+
+#endif //SPENDTRACKER_ACCOUNT_H
