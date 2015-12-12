d03f5ed29e76a478db8b42ddb1bca08c86323e3f
jun159 <baojun159@gmail.com>
2015-12-12 22:17:29 +0800

    Storage can load and save data

--- storage/Storage.cpp
+++ storage/Storage.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "Storage.h"
+
+Storage::Storage() {
+    // TODO Auto-generated constructor stub
+
+}
+
+Storage::~Storage() {
+    // TODO Auto-generated destructor stub
+}
+
+std::vector<Account> Storage::load() {
+    std::vector<Account> lol;
+    return storageLoad.execute(lol);
+}
+
+void Storage::save(std::vector<Account> allData) {
+    storageSave.execute(allData);
+}
--- storage/Storage.h
+++ storage/Storage.h
@@ -2,12 +2,25 @@
 // Created by Bao Jun on 12/12/15.
 //
 
+#include <vector>
+#include "StorageLoad.h"
+#include "StorageSave.h"
+
 #ifndef SPENDTRACKER_STORAGE_H
 #define SPENDTRACKER_STORAGE_H
 
 
 class Storage {
+public:
+    Storage();
+    virtual ~Storage();
 
+    std::vector<Account> load();
+    void save(std::vector<Account> allData);
+
+private:
+    StorageLoad storageLoad;
+    StorageSave storageSave;
 };
 
 
